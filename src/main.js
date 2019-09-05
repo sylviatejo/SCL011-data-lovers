1d1e092059615bb251085b3625806782e6841010
marielysis <marielys11@yahoo.com>
2019-09-05 12:30:05 -0400

    Imprimiendo pokemon solo imagen

--- src/main.js
+++ src/main.js
@@ -5,15 +5,18 @@ const allPokeData = POKEMON.pokemon;
 //asignar evento al boton pokemon comunes para filtrar y calcular
 const buttonPokeComm = document.getElementById("pokeComm")
     //Asignar a una variable el resultado del filtro pokemones comunes que fue un nuevo arreglo de 41 pokemones
-const printPokeComm = window.filterPoke.filterPokeCommon(allPokeData).resultFilterCommons;
+const printPokeComm = window.filterPoke.filterPokeCommon(allPokeData);
+const container = document.getElementById("principal");
 
 
 
 buttonPokeComm.addEventListener("click", () => {
-    document.getElementById("principal").innerHTML = printPokeComm;
+    //document.getElementById("principal").innerHTML = printPokeComm;
 
     //recorrer el nuevo arreglo de 41 pokemones que esta guardado en la variable printPokeComm
-    for ()
+    printPokeComm.forEach(element => {
+        container.innerHTML += `<img src = ${element.img}>`
+    })
 
 })
 
