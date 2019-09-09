ba644234c09d775f7728053ea01dbd10240f5caf
marielysis <marielys11@yahoo.com>
2019-09-08 21:40:52 -0300

    Funcionando boton poke inuasuales

--- src/main.js
+++ src/main.js
@@ -7,19 +7,18 @@ const allPokeData = POKEMON.pokemon;
 const buttonPokeComm = document.getElementById("pokeComm");
 
 //Asignar a una variable el resultado del filtro pokemones comunes que fue un nuevo arreglo de 41 pokemones
-const printPokeComm = window.filterPoke.filterPokeCommon(allPokeData);
+const printPokeComm = window.data.filterPokeCommon(allPokeData);
+console.log(printPokeComm);
 
 //asigna div root a variable container principal como padre
 const container = document.getElementById("principal");
 
-// const prinMediaPoke = window.filterPoke.mediaPokeCal(allPokeData);
 
 
 buttonPokeComm.addEventListener("click", () => {
 
 
-
-    //recorrer el nuevo arreglo de 41 pokemones que esta guardado en la variable printPokeComm
+    //recorrer el nuevo arreglo de pokemones que esta guardado en la variable printPokeComm
     printPokeComm.forEach(element => {
         //Creando el elemento div que contiene la mini tarjeta pokemon
         let createMiniPokeCars = document.createElement("div");
@@ -33,9 +32,39 @@ buttonPokeComm.addEventListener("click", () => {
         createMiniPokeCars.innerHTML += `<img src = ${element.img}>
                                         <h1>${element.name}</h1>
                                         <p>Spawn ${element.avg_spawns}</p>`
-    })
+    });
 
-})
+});
+
+
+//asignar evento al boton pokemon raros para filtrar y calcular
+const buttonPokeRare = document.getElementById("pokeRare");
+
+//Asignar a una variable el resultado del filtro pokemones inusuales que fue un nuevo arreglo de 41 pokemones
+const printPokeRare = window.data.filterPokeRare(allPokeData);
+console.log(printPokeRare);
+
+
+buttonPokeRare.addEventListener("click", () => {
+
+
+    //recorrer el nuevo arreglo de  pokemones que esta guardado en la variable printPokeComm
+    printPokeRare.forEach(element => {
+        //Creando el elemento div que contiene la mini tarjeta pokemon
+        let createMiniPokeCars = document.createElement("div");
+        //Asignando elemento padre al div creado con variable que es el contenedor
+        let asigPadre = document.getElementById("principal"); //deberia ser la caja pokebox
+        //asignamos atributos de clases dadas en style
+        createMiniPokeCars.setAttribute("class", "classDivMiniCard");
+        //agregamos hijo al padre con metodo appendChild
+        asigPadre.appendChild(createMiniPokeCars);
+        //Imprimo los valores de las propiedades de la data filtrada
+        createMiniPokeCars.innerHTML += `<img src = ${element.img}>
+                                        <h1>${element.name}</h1>
+                                        <p>Spawn ${element.avg_spawns}</p>`
+    });
+
+});
 
 // refrescar index 
 
