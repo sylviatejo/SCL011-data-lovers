18b7a503a0d37175834a4d59b38102a74f39867e
marielysis <marielys11@yahoo.com>
2019-09-10 17:25:34 -0300

    modificar select html tratando de ocultar botones

--- src/main.js
+++ src/main.js
@@ -1,5 +1,3 @@
-/* Manejo del DOM */
-
 //asignar a una variable total de data pokemon.js
 const allPokeData = window.POKEMON.pokemon;
 
@@ -13,9 +11,17 @@ const printPokeComm = window.data.filterPokeCommon(allPokeData);
 //asigna div root a variable container principal como padre
 const container = document.getElementById("principal");
 
+const buttonBlock = document.getElementById("divbuttons");
+const containerPoke = document.getElementById("pokeComm")
+containerPoke.addEventListener("click", () => {
+    buttonBlock.style.display = "block";
+});
+
+
+
 buttonPokeComm.addEventListener("click", () => {
 
-
+    document.getElementById("principal").innerHTML = "";
 
     //recorrer el nuevo arreglo de pokemones que esta guardado en la variable printPokeComm
     printPokeComm.forEach(element => {
@@ -38,6 +44,8 @@ buttonPokeComm.addEventListener("click", () => {
 });
 
 
+
+
 //asignar evento al boton pokemon raros para filtrar y calcular
 const buttonPokeRare = document.getElementById("pokeRare");
 
@@ -48,6 +56,7 @@ const printPokeRare = window.data.filterPokeRare(allPokeData);
 
 buttonPokeRare.addEventListener("click", () => {
 
+    document.getElementById("principal").innerHTML = "";
 
     //recorrer el nuevo arreglo de  pokemones que esta guardado en la variable printPokeComm
     printPokeRare.forEach(element => {
@@ -69,58 +78,32 @@ buttonPokeRare.addEventListener("click", () => {
 
 });
 
-
 /*
-function crearSelect() {
-    //crear select de ordenar 
-    let createSelect = document.createElement("select");
-    //deberia ir un for para crear las opciones
-    let padreSelect = document.getElementById("principal");
-    //Atributos del select
-    createSelect.setAttribute("id", "sortPoke");
-    //agregamos hijo al padre con metodo appendChild
-    padreSelect.appendChild(createSelect);
 
-    //crear option de select 
-    let createOptionSelect = document.createElement("option");
-    //deberia ir un for para crear las opciones
-    let padreOptionSelect = document.getElementById("sortPoke");
-    let textoOption = document.createTextNode("Por Nombre");
-    createOptionSelect.appendChild(textoOption);
-    //Atributos de option
-    createOptionSelect.setAttribute("value", "name");
-    //agregamos hijo al padre con metodo appendChild
-    padreOptionSelect.appendChild(createOptionSelect);
-
-    let valueName = document.getElementById("sortPoke").value;
-    console.log(valueName);
-    container.innerHTML = window.data.sortFilterPokeCommon(valueName);
-
-}
-*/
-
-const contOrderPoke = document.getElementById("idSelect");
-const selectOrderPoke = document.getElementById("sortPoke");
-selectOrderPoke.addEventListener("change", () => {
-    let valueOrder = document.getElementById("sortPoke").value;
-
+const OrderPoke = document.getElementById("idSelect");
+OrderPoke.addEventListener("change", () => {
+    let valueOrder = document.getElementById("idSelect").value;
     contOrderPoke.innerHTML = window.data.sortFilterPokeCommon(allPokeData, valueOrder);
-});
+
+
+    //recorrer el nuevo arreglo de pokemones que esta guardado en la variable printPokeComm
+    contOrderPoke.forEach(element => {
+        //Creando el elemento div que contiene la mini tarjeta pokemon
+        let createMiniPokeCars = document.createElement("div");
+        //Asignando elemento padre al div creado con variable que es el contenedor
+        let asigPadre = document.getElementById("principal"); //deberia ser la caja pokebox
+        //asignamos atributos de clases dadas en style
+        createMiniPokeCars.setAttribute("class", "classDivMiniCard");
+        //agregamos hijo al padre con metodo appendChild
+        asigPadre.appendChild(createMiniPokeCars);
+        //Imprimo los valores de las propiedades de la data filtrada
+        createMiniPokeCars.innerHTML += `<img src = ${element.img}>
+                                        <h1>${element.name}</h1>
+                                        <p>Spawn % ${element.spawn_chance}</p>
+                                        <p>Hora ${element.spawn_time}</p>`
+    })
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-// refrescar index 
-//document.getElementById("clear").addEventListener('click', () => {
-//location.reload()
-//});
+});*/
