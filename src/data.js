f1d2c59c582c965d09f8ff36f1bd5925940b73cb
marielysis <marielys11@yahoo.com>
2019-09-10 23:17:54 -0300

    separe funciones en data para intentar ordenar

--- src/data.js
+++ src/data.js
@@ -2,42 +2,37 @@
 
 //funcion filtrar
 window.data = {
-
-
-    filterPokeCommon: (allPokeData) => {
+    //calculo de media para filtrar comunes y raros
+    pokeMedia: (allPokeData) => {
         //console.log(allPokeData);
 
+        //convierte un array de objetos en un array numerico
         const avgSpawns = allPokeData.map(elementAvg => elementAvg.avg_spawns)
             //console.log(avgSpawns);
         let acumAvgSpawns = 0;
+        //para llenar el array con los numeros del nuevo array
         for (let i = 0; i < avgSpawns.length; i++) {
             acumAvgSpawns += avgSpawns[i];
         }
         let mediaPoke = acumAvgSpawns / avgSpawns.length;
         //console.log(mediaPoke);
+        return mediaPoke;
+
+    },
+
+
+
+    filterPokeCommon: (allPokeData, mediaPoke) => {
 
         const resultFilterCommons = allPokeData.filter(elementObject => {
             return filterPoke = elementObject.avg_spawns > mediaPoke;
             //console.log(filterPoke);
 
         });
-
-
-        return resultFilterCommons; //nuevo arreglo de pokemones mayor a al promedio con todos sus objetos
-
+        return resultFilterCommons; //nuevo arreglo de pokemones mayor a la media
     },
 
-    filterPokeRare: (allPokeData) => {
-        //console.log(allPokeData);
-
-        const avgSpawns = allPokeData.map(elementAvg => elementAvg.avg_spawns)
-            //console.log(avgSpawns);
-        let acumAvgSpawns = 0;
-        for (let i = 0; i < avgSpawns.length; i++) {
-            acumAvgSpawns += avgSpawns[i];
-        }
-        let mediaPoke = acumAvgSpawns / avgSpawns.length;
-        //console.log(mediaPoke);
+    filterPokeRare: (allPokeData, mediaPoke) => {
 
         const resultFilterRare = allPokeData.filter(elementObject => {
             return filterPoke = elementObject.avg_spawns < mediaPoke;
@@ -46,27 +41,26 @@ window.data = {
         });
         //console.log(resultFilterCommons);
 
-        return resultFilterRare; //nuevo arreglo de pokemones mayor a al promedio con todos sus objetos
-
+        return resultFilterRare; //nuevo arreglo de pokemones menor a la media
     },
 
 
 
 
 
-    /*
-        sortFilterPokeCommon: (allPokeData, sortby, sortPoke) => {
 
-            let sortPoke = allPokeData.sort(function(a, b) {
-                if (a.name > b.name) {
-                    return 1;
-                }
-                if (a.name < b.name) {
-                    return -1;
-                }
+    sortPokemon: (resultFilterCommons, sortPoke) => {
 
-            });
-            //console.log(sortPoke);
-        }*/
+        sortPoke = resultFilterCommons.sort(function(a, b) {
+            if (a.name > b.name) {
+                return 1;
+            }
+            if (a.name < b.name) {
+                return -1;
+            }
+
+        });
+        console.log(sortPoke);
+    }
 
 };
--- src/main.js
+++ src/main.js
@@ -1,21 +1,31 @@
 //asignar a una variable total de data pokemon.js
 const allPokeData = window.POKEMON.pokemon;
+const mediaPoke = window.data.pokeMedia(allPokeData);
+
+
 
 //asignar evento al boton pokemon comunes para filtrar y calcular
 const buttonPokeComm = document.getElementById("pokeComm");
 
 //Asignar a una variable el resultado del filtro pokemones comunes que fue un nuevo arreglo de 41 pokemones
-const printPokeComm = window.data.filterPokeCommon(allPokeData);
-//console.log(printPokeComm);
+const printPokeComm = window.data.filterPokeCommon(allPokeData, mediaPoke);
+console.log(printPokeComm);
 
-//asigna div root a variable container principal como padre
+//asignar evento al boton pokemon raros para filtrar y calcular
+const buttonPokeRare = document.getElementById("pokeRare");
+
+//Asignar a una variable el resultado del filtro pokemones inusuales que fue un nuevo arreglo de 41 pokemones
+const printPokeRare = window.data.filterPokeRare(allPokeData, mediaPoke);
+//console.log(printPokeRare);
+
+/*asigna div root a variable container principal como padre
 const container = document.getElementById("principal");
 
 const buttonBlock = document.getElementById("divbuttons");
 const containerPoke = document.getElementById("pokeComm")
 containerPoke.addEventListener("click", () => {
     buttonBlock.style.display = "block";
-});
+});*/
 
 
 
@@ -45,15 +55,6 @@ buttonPokeComm.addEventListener("click", () => {
 
 
 
-
-//asignar evento al boton pokemon raros para filtrar y calcular
-const buttonPokeRare = document.getElementById("pokeRare");
-
-//Asignar a una variable el resultado del filtro pokemones inusuales que fue un nuevo arreglo de 41 pokemones
-const printPokeRare = window.data.filterPokeRare(allPokeData);
-//console.log(printPokeRare);
-
-
 buttonPokeRare.addEventListener("click", () => {
 
     document.getElementById("principal").innerHTML = "";
@@ -78,32 +79,33 @@ buttonPokeRare.addEventListener("click", () => {
 
 });
 
-/*
 
-const OrderPoke = document.getElementById("idSelect");
-OrderPoke.addEventListener("change", () => {
-    let valueOrder = document.getElementById("idSelect").value;
-    contOrderPoke.innerHTML = window.data.sortFilterPokeCommon(allPokeData, valueOrder);
+// ordenar
+document.getElementById("sortPokemon").addEventListener("change", ordenPoke);
+document.getElementById("principal").innerHTML = "";
+
+function ordenPoke() {
+    //contenedor de las card ordenadas
+    const contOrderPoke = document.getElementById("principal");
+    //asiganr variable del valor de select
+    let sorPoke = document.getElementById("sortPokemon").value;
+
+    let ordenPokemonEnd = window.data.sortPokemon(printPokeComm, sorPoke);
+
+    //imprimir
+
+    document.getElementById("principal").innerHTML = "";
+
+    ordenPokemonEnd.forEach(element => {
+
+        contOrderPoke.innerHTML +=
+            `<div class="classDivMiniCard">
+            <img src = ${element.img}>
+            <h1>${element.name}</h1>
+            <p>Spawn % ${element.spawn_chance}</p>
+            <p>Hora ${element.spawn_time}</p>
+            </div>`
+    });
 
 
-    //recorrer el nuevo arreglo de pokemones que esta guardado en la variable printPokeComm
-    contOrderPoke.forEach(element => {
-        //Creando el elemento div que contiene la mini tarjeta pokemon
-        let createMiniPokeCars = document.createElement("div");
-        //Asignando elemento padre al div creado con variable que es el contenedor
-        let asigPadre = document.getElementById("principal"); //deberia ser la caja pokebox
-        //asignamos atributos de clases dadas en style
-        createMiniPokeCars.setAttribute("class", "classDivMiniCard");
-        //agregamos hijo al padre con metodo appendChild
-        asigPadre.appendChild(createMiniPokeCars);
-        //Imprimo los valores de las propiedades de la data filtrada
-        createMiniPokeCars.innerHTML += `<img src = ${element.img}>
-                                        <h1>${element.name}</h1>
-                                        <p>Spawn % ${element.spawn_chance}</p>
-                                        <p>Hora ${element.spawn_time}</p>`
-    })
-
-
-
-
-});*/
+};
